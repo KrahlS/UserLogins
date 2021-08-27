20ba9026bf33eb9f16c48ef3c49f5395948d144a
Scott Krahl <83058231+KrahlS@users.noreply.github.com>
2021-08-27 17:45:01 -0500

    Working prototype of user login system

--- UserLogins.java
+++ UserLogins.java
@@ -1,151 +1,202 @@
+
 import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-import java.util.ArrayList;
+import java.sql.*;
 import java.util.Scanner;
 
 public class UserLogins {
 
-	private static ArrayList<User> userArray = new ArrayList<User>();
-	private static Connection c = null; 
-	private static Statement stmt = null; 
+  private static Connection c = null;
+  private static Statement stmt = null;
+  private static String sql = null;
 
-	public static void addUser() {
-		String username = "";
-		String password = "";
-		Scanner scnr = new Scanner(System.in);
+  public static void addUser() {
+    String username = "";
+    String password = "";
+    Scanner scnr = new Scanner(System.in);
 
-		System.out.println("Enter desired username:\n>");
-		username = scnr.next();
+    System.out.print("Enter desired username:\n>");
+    username = scnr.next();
 
-		System.out.println("Enter new password:\n>");
-		password = getHash(scnr.next());
+    try {
 
-		try {
-			Class.forName("org.sqlite.JDBC");
-			c = DriverManager.getConnection("jdbc:sqlite:ppab6.db");
-			stmt = c.createStatement();
-			String sql = "INSERT INTO users (username)\n" + "VALUES(" + username + ");";
-			stmt.executeUpdate(sql);
-			sql = "INSERT INTO users (password_hash)\n" + "VALUES(" + password + ");";
-			stmt.executeUpdate(sql);
-			stmt.close();
-			// c.commit();
-			c.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.err.println("Error connecting to SQLite database");
-			System.exit(0);
-		}
-		System.out.println("User added successfully");
-	}
+      // Check if username is taken
 
-	public static void setupDB() {
+      boolean userExists = checkUserExists(username);
 
-		Scanner scnr = new Scanner(System.in);
-		char userIn; 
-		
-		try {
-			File file = new File("ppab6.db");
+      while (userExists) {
+        System.out.print("That name is already taken. Please try something different.\n>");
+        username = scnr.next();
+        userExists = checkUserExists(username);
+      }
 
-			if (file.exists()) // here's how to check
-			{
-				System.out.println("This database name already exists");
-				System.out.print("Would you like to delete and recreate it? (y/n)\n>");
-				userIn = scnr.nextLine().toUpperCase().charAt(0);
-				if(userIn == 'N') {
-					return; 
-				} else if (userIn == 'Y') {
-					if(file.delete()) {
-						System.out.println("Database deleted successfully.");
-					} else {
-						System.out.println("A problem was encountered while deleting the database.");
-					}
-				} else {
-					System.out.println("Invalid input. Returning to main");
-				}
-					
-			} 
+      System.out.print("Enter new password:\n>");
+      password = getHash(scnr.next());
 
-				Class.forName("org.sqlite.JDBC");
-				c = DriverManager.getConnection("jdbc:sqlite:ppab6.db");
-				stmt = c.createStatement();
-				String sql = "CREATE TABLE users (username VARCHAR, password_hash VARCHAR);";
-				stmt.executeUpdate(sql);
-				stmt.close();
-				c.close();
-				System.out.println("Successfully created database");
-			
+      sql = "INSERT INTO users (username,password_hash) VALUES('" + username + "','" + password
+          + "');";
+      System.out.println("Added user: " + username);
+      System.out.println("Added password hash: " + password);
+      stmt.executeUpdate(sql);
 
-		} catch (Exception e) {
-			System.err.println("Error connecting to SQLite database");
-			System.exit(0);
-		}
-		
-	}
+      System.out.println("User added successfully");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 
-	private static boolean isValidCredentials(String username, String password) {
-		String hashedPass = getHash(password);
-		for (int i = 0; i < userArray.size(); i++) {
-			User validUser = userArray.get(i);
-			if (username.equals(validUser.getUsername())) {
-				if (hashedPass.equals(validUser.getPassword()))
-					return true;
-			}
-		}
-		return false;
-	}
+  public static boolean checkUserExists(String username) {
+    try {
+      sql = "SELECT username FROM users";
+      ResultSet rs = stmt.executeQuery(sql);
+      while (rs.next()) {
+        if (rs.getString("username").equals(username)) {
+          return true;
+        }
+      }
+      return false;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      return false;
+    }
+  }
 
-	private static String bytesToHex(byte[] hash) {
-		StringBuilder hexString = new StringBuilder(2 * hash.length);
-		for (int i = 0; i < hash.length; i++) {
-			String hex = Integer.toHexString(0xff & hash[i]);
-			if (hex.length() == 1) {
-				hexString.append('0');
-			}
-			hexString.append(hex);
-		}
-		return hexString.toString();
-	}
+  public static void createNewDatabase(String fileName) {
 
-	public static String getHash(String plaintext) {
-		MessageDigest digest;
-		try {
-			digest = MessageDigest.getInstance("SHA-256");
-			byte[] encodedhash = digest.digest(plaintext.getBytes(StandardCharsets.UTF_8));
-			String hexValue = bytesToHex(encodedhash);
-			return hexValue;
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
+    String url = "jdbc:sqlite:C:/sqlite/db/" + fileName;
 
-	public static void main(String[] args) {
+    try (Connection conn = DriverManager.getConnection(url)) {
+      if (conn != null) {
+        DatabaseMetaData meta = conn.getMetaData();
+        System.out.println("The driver name is " + meta.getDriverName());
+        System.out.println("A new database has been created.");
+      }
 
-		/*
-		 * User user1 = new User("user1", "password1"); User user2 = new User("user2",
-		 * "password2"); User user3 = new User("user3", "password3");
-		 * 
-		 * userArray.add(user1); userArray.add(user2); userArray.add(user3);
-		 * 
-		 * 
-		 * Scanner scnr = new Scanner(System.in);
-		 * System.out.print("Enter username:\n>"); String username = scnr.next();
-		 * System.out.print("Enter password:\n>"); String password = scnr.next();
-		 * 
-		 * if (isValidCredentials(username, password)) {
-		 * System.out.println("Deep dark secret!"); } else {
-		 * System.out.println("Invalid username or password\nGet lost!"); }
-		 */
-		Scanner scnr = new Scanner(System.in);
-		setupDB();
-		addUser();
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+    }
+  }
 
-	}
+
+  public static void setupDB() {
+
+    Scanner scnr = new Scanner(System.in);
+    char userIn;
+    String dbName = "userData.db";
+
+    try {
+
+      c = DbConnection.connect(dbName);
+
+      stmt = c.createStatement();
+      DatabaseMetaData dbm = c.getMetaData();
+
+      // check for previous data
+      ResultSet tables = dbm.getTables(null, null, "users", null);
+      if (tables.next()) {
+        System.out.print("The database contains previous data. Override? (y/n)\n>");
+        userIn = scnr.next().toLowerCase().charAt(0);
+        if (userIn == 'y') {
+          sql = "DELETE FROM 'users'";
+          stmt.executeUpdate(sql);
+
+
+          System.out.println("Data has been overwritten.");
+        } else {
+          System.out.println("OK. Proceeding with previous data.");
+        }
+      }
+
+      sql =
+          "CREATE TABLE IF NOT EXISTS \"users\" (\"username\" VARCHAR, \"password_hash\" VARCHAR);";
+      stmt.execute(sql);
+
+      System.out.println("Successfully set up database!");
+
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+
+  }
+
+
+
+  private static boolean isValidCredentials(String username, String password) {
+    String hashedPass = getHash(password);
+    
+    try {
+      sql = "SELECT username, password_hash FROM users"; 
+      stmt = c.createStatement();
+      ResultSet rs = stmt.executeQuery(sql); 
+      
+      while(rs.next()) {
+        if (rs.getString("username").equals(username)) {
+          if (rs.getString("password_hash").equals(hashedPass)) {
+            return true;
+          }
+        }
+      }
+      System.out.println("Incorrect username or password.\nGet lost!"); 
+      return false;
+    } catch (SQLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } 
+    
+    
+    return false;
+  }
+
+  private static String bytesToHex(byte[] hash) {
+    StringBuilder hexString = new StringBuilder(2 * hash.length);
+    for (int i = 0; i < hash.length; i++) {
+      String hex = Integer.toHexString(0xff & hash[i]);
+      if (hex.length() == 1) {
+        hexString.append('0');
+      }
+      hexString.append(hex);
+    }
+    return hexString.toString();
+  }
+
+  public static String getHash(String plaintext) {
+    MessageDigest digest;
+    try {
+      digest = MessageDigest.getInstance("SHA-256");
+      byte[] encodedhash = digest.digest(plaintext.getBytes(StandardCharsets.UTF_8));
+      String hexValue = bytesToHex(encodedhash);
+      return hexValue;
+    } catch (NoSuchAlgorithmException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  public static void main(String[] args) {
+
+    Scanner scnr = new Scanner(System.in);
+
+    try {
+      setupDB();
+      addUser();
+      
+      System.out.print("Enter your username:\n>"); 
+      String username = scnr.next(); 
+      System.out.print("Enter your password:\nn>"); 
+      String password = scnr.next(); 
+      
+      System.out.println(isValidCredentials(username, password)); 
+      
+      stmt.close();
+      c.close();
+    } catch (SQLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+
+  }
 
 }
